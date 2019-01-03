48629c19dc3fa6efc197d5b8cd773bf634221199
ido.ad.se@gmail.com <ido.ad.se@gmail.com>
2019-01-03 19:03:16 +0200

    more comfortable read by removing nowrap, so all content visible on first page. ignore *.zip files

--- harvard_comments/replace.js
+++ harvard_comments/replace.js
@@ -1,9 +1,8 @@
-function addTitles() {
-	var elems = document.getElementsByClassName("forum-nav-thread-wrapper-1");
+function addComments() {
+    var elems = document.getElementsByClassName("thread-preview-body");
 	for (var i=0; i<elems.length; i++) {
-		var title = document.getElementsByClassName("forum-nav-thread-wrapper-1")[i].getElementsByClassName("forum-nav-thread-title")[0].innerText + ":  \n" + document.getElementsByClassName("forum-nav-thread-wrapper-1")[i].getElementsByClassName("thread-preview-body")[0].innerText
-		elems[i].title = title
+		var title = document.getElementsByClassName("thread-preview-body")[i].style["white-space"]="normal"
 	}
 }
 
-setInterval(addTitles, 1000)
+setInterval(addComments, 1000)
